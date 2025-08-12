366b323b206de80b3127cccbeb82d52f83c2fbb8
Thanyang <Theanyang@example.com>
2025-08-12 19:16:28 +0700

    Describe what you changed

--- app.py
+++ app.py
@@ -1988,7 +1988,8 @@ def index():
     return render_template('front/contact.html')
   # your Jinja template file
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
